c6878e1fc4bd0729dcc6c53d613d9ace40dcd706
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2023-10-06 03:56:14 +0200

    06-10-2023

--- src/Server/Connections/Connection.fs
+++ src/Server/Connections/Connection.fs
@@ -12,10 +12,10 @@ module Connection =
     //let [<Literal>] connStringSomee = @"" 
 
     //nterapie.somee.com //testing website
-    let [<Literal>] internal connStringSomee = @"workstation id=nterapie.mssql.somee.com;packet size=4096;user id=FSharpDeveloper_SQLLogin_1;pwd=1791iyi6tf;data source=nterapie.mssql.somee.com;persist security info=False;initial catalog=nterapie" 
+    let [<Literal>] internal connStringSomee = @":-)" 
 
     //localhost
-    //let [<Literal>] internal connStringLocal = @"Data Source=Misa\SQLEXPRESS;Initial Catalog=nterapieLocal;Integrated Security=True"
+    //let [<Literal>] internal connStringLocal = @":-)"
 
     let internal getConnection () =
         let connection = new SqlConnection(connStringSomee)
