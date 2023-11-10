cd2dcff7fabb35b619235e8e6005ee999925544f
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2023-11-10 16:16:25 +0100

    10-11-2023

--- src/Server/Connections/Connection.fs
+++ src/Server/Connections/Connection.fs
@@ -12,7 +12,7 @@ module Connection =
     //let [<Literal>] connStringSomee = @"" 
 
     //nterapie.somee.com //testing website
-    let [<Literal>] internal connStringSomee = @"workstation id=nterapie.mssql.somee.com;packet size=4096;user id=FSharpDeveloper_SQLLogin_1;pwd=1791iyi6tf;data source=nterapie.mssql.somee.com;persist security info=False;initial catalog=nterapie" 
+    let [<Literal>] internal connStringSomee = @"" 
 
     //localhost
     //let [<Literal>] internal connStringLocal = @"Data Source=Misa\SQLEXPRESS;Initial Catalog=nterapieLocal;Integrated Security=True"
